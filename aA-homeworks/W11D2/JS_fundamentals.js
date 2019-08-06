afbf1d17341b716069fc257b9f5e35fa6e7e87c7
ansaroyan <anoushsaroyan@gmail.com>
2019-08-06 09:20:19 -0700

    Added more js problems: part 2

--- aA-homeworks/W11D2/JS_fundamentals.js
+++ aA-homeworks/W11D2/JS_fundamentals.js
@@ -1,3 +1,4 @@
+// Part 1
 
 // Phase I
 // mystery scoping
@@ -72,7 +73,7 @@ function isSubstring(phrase, subphrase) {
     return phrase.includes(subphrase);
 }
 
-isSubstring("time to program", "time");
+// isSubstring("time to program", "time");
 
 // Phase II
 
@@ -90,7 +91,7 @@ function fizzBuzz(array) {
     return fizzBuzzArr;
 }
 
-fizzBuzz([50, 3, 15, 30, 5])
+// fizzBuzz([50, 3, 15, 30, 5])
 
 // isPrime
 
@@ -106,7 +107,7 @@ function isPrime(n) {
     return true;
 }
 
-isPrime(15485863);
+// isPrime(15485863);
 
 // sumOfNPrimes
 
@@ -126,4 +127,76 @@ function sumOfNPrimes(n) {
     return sum;
 }
 
-sumOfNPrimes(4);
+// sumOfNPrimes(4);
+
+
+// Part 2
+
+function titleize(names, callback) {
+    let titleized = names.map(name => `Mx. ${name} Jingleheimer Schmidt`);
+    callback(titleized);
+};
+
+// call
+// titleize(["Mary", "Brian", "Leo"], (names) => {
+//     names.forEach(name => console.log(name));
+// });
+
+// Phase II - Constructors, Prototypes, and `this`
+
+function Elephant(name, height, tricks) {
+    this.name = name;
+    this.height = height;
+    this.tricks = tricks;
+}
+
+Elephant.prototype.trumpet = function () {
+    console.log(`${this.name} the elephant goes 'phrRRRRRRRRRRR!!!!!!!'`);
+};
+
+Elephant.prototype.grow = function () {
+    this.height = this.height + 12;
+};
+
+Elephant.prototype.addTrick = function (trick) {
+    this.tricks.push(trick);
+};
+
+Elephant.prototype.play = function () {
+    trickIndex = Math.floor(Math.random() * this.tricks.length);
+    console.log(`${this.name} is ${this.tricks[trickIndex]}!`);
+};
+
+// call
+// let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+// let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+// let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+// let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+// let herd = [ellie, charlie, kate, micah];
+
+// Phase III - Function Invocation
+
+Elephant.paradeHelper = function (elephant) {
+    console.log(`${elephant.name} is trotting by!`);
+};
+
+// Elephant.paradeHelper(micah);
+
+
+// Phase IV - Closures
+
+function dinerBreakfast() {
+    let order = "I'd like cheesy scrambled eggs please.";
+    console.log(order);
+
+    return function (food) {
+        order = `${order.slice(0, order.length - 8)} and ${food} please.`;
+        console.log(order);
+    };
+};
+
+// call
+// let bfastOrder = dinerBreakfast();
+// bfastOrder("chocolate chip pancakes");
+// bfastOrder("grits");
